b6ef0825c05e440d3b764d9464295d8636f62ee8
Sally Young <sally@justafish.co.uk>
2017-10-17 12:23:05 -0700

    Upgrade code to modern JS

--- app/public/javascripts/facets.js
+++ app/public/javascripts/facets.js
@@ -1,15 +1,14 @@
 /**
  * Adds behaviors to toggle facets.
  */
-var Anchors = document.getElementsByClassName('facet');
+const Anchors = document.getElementsByClassName('facet');
 
-for (var i = 0; i < Anchors.length ; i++) {
-  Anchors[i].addEventListener('click',
-    function (event) {
+Anchors.forEach(anchor => {
+  anchor.addEventListener('click', (event) => {
       event.preventDefault();
-      var value = this.getAttribute('data-value');
-      var old_value = document.getElementById('type').getAttribute('value');
-      if ((old_value.length === undefined) || (old_value === '')) {
+      const value = this.getAttribute('data-value');
+      const oldValue = document.getElementById('type').getAttribute('value');
+      if (!oldValue || oldValue.length === 0) {
         document.getElementById('type').setAttribute('value', value);
       }
       else {
@@ -18,4 +17,4 @@ for (var i = 0; i < Anchors.length ; i++) {
       document.getElementById("search_form").submit();
     },
     false);
-}
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,7 @@
-var express = require('express')
-var router = express.Router()
+const elasticsearch = require('elasticsearch');
+const express = require('express');
 
-/**
- * Helper to check if an object's property does not exist or is empty.
- *
- * @see https://stackoverflow.com/a/28552610/1120652
- *
- * @param {Object} object
- *   The object whose property will be checked.
- * @param {string} property
- *   The property within the object to check.
- * @returns {boolean}
- *   TRUE if the property does not exist or is empty.
- */
-var isEmpty = function (object, property) {
-  return ((object[property] === undefined) ||
-    (object[property].length === 0) ||
-    (object[property] === 'undefined'))
-}
+const router = express.Router();
 
 /**
  * Performs a query against Elasticsearch and renders
@@ -28,89 +12,90 @@ var isEmpty = function (object, property) {
  * @param {Object} res
  *   The response object.
  */
-var doSearch = function (req, res) {
+const doSearch = (req, res) => {
   // Initialize the Elasticsearch client.
-  var elasticsearch = require('elasticsearch')
-  var client = new elasticsearch.Client({
+  const client = new elasticsearch.Client({
     host: 'http://elastic:changeme@elasticsearch:9200',
-    log: 'trace'
-  })
-  var search_string = ''
-  var search_type = ''
+    log: 'trace',
+  });
+  let searchString = '';
+  let searchType = '';
 
   // Prepare the request body.
-  var body = {
-    'size': 100
-  }
-  if (!isEmpty(req.query, 'search') || !isEmpty(req.query, 'type')) {
-    var query = {
-      'bool': {}
-    }
+  const body = {
+    size: 100,
+  };
 
-    if (!isEmpty(req.query, 'search')) {
+  if (!req.query.search || !req.query.type) {
+    const query = {
+      bool: {},
+    };
+
+    if (!req.query.search) {
       query.bool.must = {
-        'multi_match': {
-          'fields': [
+        multi_match: {
+          fields: [
             'title^2',
-            'summary'
+            'summary',
           ],
-          'query': req.query.search,
-          'fuzziness': 'auto'
-        }
-      }
-      search_string = req.query.search
+          query: req.query.search,
+          fuzziness: 'auto',
+        },
+      };
+      searchString = req.query.search;
     }
 
-    if (!isEmpty(req.query, 'type')) {
+    if (!req.query.type) {
       query.bool.filter = [
         {
-          'term': {
-            'type': req.query.type
-          }
-        }
-      ]
-      search_type = req.query.type
+          term: {
+            type: req.query.type,
+          },
+        },
+      ];
+      searchType = req.query.type;
     }
 
-    body.query = query
+    body.query = query;
   }
 
   // Add a type facet.
   body.aggs = {
-    'type': {
-      'terms': {
-        'field': 'type'
-      }
-    }
-  }
+    type: {
+      terms: {
+        field: 'type',
+      },
+    },
+  };
 
   // Perform the search request.
   client.search({
     index: 'elasticsearch_index_draco_elastic',
-    body: body
-  }).then(function (resp) {
-    var hits = resp.hits.hits
-    var total = resp.hits.total
-    // Render results in a template.
-    res.render('index', {
-      title: 'Node.js Elasticsearch demo',
-      hits: hits,
-      total: total,
-      aggregations: resp.aggregations.type.buckets,
-      search_string: search_string,
-      search_type: search_type
-    })
-  }, function (err) {
-    console.trace(err.message)
+    body,
   })
-}
+    .then((resp) => {
+      const { hits } = resp.hits;
+      const { total } = resp.hits;
+      // Render results in a template.
+      res.render('index', {
+        title: 'Node.js Elasticsearch demo',
+        hits,
+        total,
+        aggregations: resp.aggregations.type.buckets,
+        search_string: searchString,
+        search_type: searchType,
+      });
+    }, (err) => {
+      console.trace(err.message); // eslint-disable-line no-console
+    });
+};
 
-router.get('/', function (req, res) {
-  doSearch(req, res)
-})
+router.get('/', (req, res) => {
+  doSearch(req, res);
+});
 
-router.post('/', function (req, res) {
-  doSearch(req, res)
-})
+router.post('/', (req, res) => {
+  doSearch(req, res);
+});
 
-module.exports = router
+module.exports = router;
