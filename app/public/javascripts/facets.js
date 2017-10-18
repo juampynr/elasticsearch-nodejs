881325eb1237512cbd2cdbc64872a350b2045a80
Juampy <juampynr@gmail.com>
2017-10-18 11:53:15 +0200

    Document the facet's JavaScript

--- app/public/javascripts/facets.js
+++ app/public/javascripts/facets.js
@@ -3,11 +3,13 @@
  */
 var Anchors = document.getElementsByClassName('facet');
 
+// Loop facets and add a click listener.
 for (var i = 0; i < Anchors.length ; i++) {
   Anchors[i].addEventListener('click',
     function (event) {
       event.preventDefault();
       var value = this.getAttribute('data-value');
+      // Either set a hidden field with the facet value or set it to an empty string.
       var old_value = document.getElementById('type').getAttribute('value');
       if ((old_value.length === undefined) || (old_value === '')) {
         document.getElementById('type').setAttribute('value', value);
@@ -15,6 +17,7 @@ for (var i = 0; i < Anchors.length ; i++) {
       else {
         document.getElementById('type').setAttribute('value', '');
       }
+      // Finally, submit the form.
       document.getElementById("search_form").submit();
     },
     false);
